f3559ac271034b931921b6d793fc2eabb7a18753
ramyamahendra332-sudo <ramyamahendra332@gmail.com>
2026-05-03 12:09:07 +0530

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,8 +1,48 @@
+const prices={
+"Pizza Hut":299,
+"KFC":249,
+"Burger King":199,
+"Dominos":279
+};
+
 function addToCart(item){
 let cart=JSON.parse(localStorage.getItem("cart"))||[];
 cart.push(item);
 localStorage.setItem("cart",JSON.stringify(cart));
-alert(item+" added to cart");
+alert(item+" added");
+}
+
+window.onload=function(){
+renderCart();
+}
+
+function renderCart(){
+let container=document.getElementById("cartContainer");
+if(!container) return;
+
+let cart=JSON.parse(localStorage.getItem("cart"))||[];
+let total=0;
+
+container.innerHTML="";
+
+cart.forEach(item=>{
+total+=prices[item];
+
+container.innerHTML+=`
+<div class="cart-item">
+<h3>${item}</h3>
+<p>₹${prices[item]}</p>
+</div>
+`;
+});
+
+document.getElementById("total").innerText=total;
+}
+
+function payNow(){
+alert("Payment Successful. Order placed!");
+localStorage.clear();
+location.reload();
 }
 
 function searchRestaurant(){
@@ -13,24 +53,4 @@ for(let i=0;i<cards.length;i++){
 let name=cards[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
 cards[i].style.display=name.includes(input)?"block":"none";
 }
-}
-
-window.onload=function(){
-let list=document.getElementById("cartItems");
-
-if(list){
-let cart=JSON.parse(localStorage.getItem("cart"))||[];
-
-cart.forEach(item=>{
-let li=document.createElement("li");
-li.innerText=item;
-list.appendChild(li);
-});
-}
-}
-
-function checkout(){
-alert("Order placed successfully!");
-localStorage.clear();
-location.reload();
 }
